ec20d16064a3ef2beb5c096991325451a1ed6e9c
Catchme <mdksankalpa@gmail.com>
2020-08-12 08:34:06 +0930

    Install Swagger to ToDoAPI

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace TodoApi
             services.AddDbContext<TodoContext>(opt =>
               opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,19 @@ namespace TodoApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
